10f7e0d072efc0afaa27e2ec6831abe557151713
Miles Gould <miles@assyrian.org.uk>
2021-03-02 11:34:56 +0000

    Add not-very-good Metropolis-Hastings solver

--- net/net.py
+++ net/net.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import fileinput
+from random import random
 
 import numpy as np
 
@@ -42,7 +43,7 @@ class Board:
 
     def set_exits(self, i, j):
         shape = shapes[self.pieces[i][j]]
-        orientation = self.orientations[i][j]
+        orientation = self.orientations[i, j]
         (self.left[i, j], self.up[i, j], self.right[i, j], self.down[i, j]) = rotate(shape, orientation)
 
     def penalties(self):
@@ -64,7 +65,40 @@ class Board:
             for i in range(self.size)
         ])
 
+    def solve(self, steps=None):
+        count = 0
+        while True:
+            if count >= steps:
+                break
+            count += 1
+            penalties = np.reshape(self.penalties(), self.size ** 2)
+            error = np.sum(penalties)
+            if error == 0:
+                # Finished!
+                break
+            index = np.random.choice(np.arange(self.size ** 2), p=(penalties / error))
+            i, j = divmod(index, self.size)
+            current = self.orientations[i, j]
+            self.orientations[i, j] = (current + 1) % 4
+            self.set_exits(i, j)
+            # if not an improvement, reject with probability new / (new + old)
+            # TODO we don't need to recalculate all penalties for this
+            old_penalty = penalties[index]
+            new_penalty = self.penalties()[i, j]
+            if new_penalty > old_penalty and random() < new_penalty / (new_penalty + old_penalty):
+                self.orientations[i, j] = current
+                self.set_exits(i, j)
+        return count
+
 
 if __name__ == '__main__':
     board = Board([line for line in fileinput.input()])
     print(board)
+    print(board.penalties())
+    print(np.sum(board.penalties()))
+    print()
+    count = board.solve(steps=500)
+    print(f"After {count} steps:")
+    print(board)
+    print(board.penalties())
+    print(np.sum(board.penalties()))
