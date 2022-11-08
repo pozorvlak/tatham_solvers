1f104c15f8ec39cb7f6f1baeae61494dca6683a6
Miles Gould <miles@assyrian.org.uk>
2022-11-08 17:56:44 +0000

    Use greedy algorithm to minimize boards
    This is how Simon Tatham's Unruly generator does it!

--- unruly/minimize_z3.py
+++ unruly/minimize_z3.py
@@ -6,9 +6,8 @@ Find Unruly boards which can't be solved using the four tactics using Z3.
 """
 
 import fileinput
-import sys
 
-from z3 import Function, IntSort, Solver, sat, And, Implies, If, Or
+from z3 import Function, IntSort, Solver, sat, Or
 
 import unruly
 from unruly_z3 import solve_board
@@ -16,93 +15,32 @@ from unruly_z3 import solve_board
 
 def minimize(board, expected_solution):
     size = len(board)
-    s = Solver()
-    puzzle = make_puzzle(size, s)
-    for i, row in enumerate(board):
-        for j, c in enumerate(row):
-            s.add(Or(puzzle(i, j) == c, puzzle(i, j) == 2))
-    solution = solution_for(puzzle, size, s)
-    lo_blanks = sum(c == 2 for row in board for c in row)
-    hi_blanks = size * size
-    blank_count = sum(b2i(puzzle(i, j) == 2) for i in range(size) for j in range(size))
-    while (hi_blanks - lo_blanks) > 1:
-        goal = (lo_blanks + hi_blanks) // 2
-        print(f"Can achieve {lo_blanks}, can't achieve {hi_blanks}, goal is {goal}")
-        s.add(blank_count > goal)
-        iterations = 0
-        while True:
-            iterations += 1
-            if s.check() == sat:
-                board = get_board(s, puzzle, size)
-                blanks = s.model().evaluate(blank_count).as_long()
-                s.push()
-                s.add(yes_this(puzzle, board, size))
-                s.add(not_this(solution, expected_solution, size))
-                if s.check() == sat:
-                    # Solution is not unique
-                    if iterations % 10 == 0:
-                        sys.stdout.write(".")
-                        sys.stdout.flush()
-                    s.pop()
-                    s.add(not_this(puzzle, board, size))
-                else:
-                    s.pop()
-                    print(f"\nFound a subset with {blanks} blanks")
-                    print(unruly.board_to_str(board))
-                    lo_blanks = blanks
-                    break
-            else:
-                print(f"Can't find any solutions with {goal} blanks")
-                hi_blanks = goal
-                break
+    clues = [
+        (i, j)
+        for i in range(size) for j in range(size)
+        if board[i][j] != 2
+    ]
+    for (i, j) in clues:
+        value = board[i][j]
+        board[i][j] = 2
+        if solve_board(board, exclusions=[expected_solution]) is not None:
+            board[i][j] = value
     return board
 
-
-def save_assertions(s):
-    with open("assertions", "w") as f:
-        for a in s.assertions():
-            f.write(str(a) + "\n")
-                
-
-def yes_this(variable, board, size):
-    return And(*(
-        variable(i, j) == board[i][j]
-        for i in range(size) for j in range(size)
-    ))
-
-
-def not_this(variable, board, size):
-    return Or(*(
-        variable(i, j) != board[i][j]
-        for i in range(size) for j in range(size)
-    ))
-
-
-def make_puzzle(size, s):
-    puzzle = Function('puzzle', IntSort(), IntSort(), IntSort())
-    # Squares must be black, white or empty
-    for i in range(size):
-        for j in range(size):
-            # 0 = black, 1 = white, 2 = unknown
-            s.add(puzzle(i, j) >= 0)
-            s.add(puzzle(i, j) <= 2)
-    return puzzle
-
-
-def b2i(bool_expr):
-    """Convert a Z3 boolean expression to an integer, so I can sum it"""
-    return If(bool_expr, 1, 0)
-
-
-def solution_for(puzzle, size, s):
-    solution = Function('solution', IntSort(), IntSort(), IntSort())
+def solve_board(board, exclusions=None):
+    if exclusions is None:
+        exclusions = []
+    size = len(board[0])
     n = size // 2
-    for i in range(size):
-        for j in range(size):
-            c = puzzle(i, j)
-            s.add(0 <= solution(i, j))
-            s.add(solution(i, j) <= 1)
-            s.add(Implies(c < 2, solution(i, j) == c))
+    solution = Function('solution', IntSort(), IntSort(), IntSort())
+    s = Solver()
+    for i, row in enumerate(board):
+        for j, c in enumerate(row):
+            if c < 2:
+                s.add(solution(i, j) == c)
+            else:
+                s.add(0 <= solution(i, j))
+                s.add(solution(i, j) <= 1)
     for i in range(size):
         s.add(sum(solution(i, j) for j in range(size)) == n)
         s.add(sum(solution(j, i) for j in range(size)) == n)
@@ -114,20 +52,26 @@ def solution_for(puzzle, size, s):
             s.add(row_sum < 3)
             s.add(0 < col_sum)
             s.add(col_sum < 3)
-    return solution
-
-
-def get_board(s, solution, size):
-    m = s.model()
-    board = [
-        [ m.evaluate(solution(i, j)).as_long() for j in range(size) ]
-        for i in range(size)
-    ]
-    return board
+    for exclusion in exclusions:
+        s.add(Or(*(
+            solution(i, j) != exclusion[i][j]
+            for i in range(size) for j in range(size)
+        )))
+    if s.check() == sat:
+        m = s.model()
+        board = [
+            [ m.evaluate(solution(i, j)).as_long() for j in range(size) ]
+            for i in range(size)
+        ]
+        return board
+    else:
+        return None
 
 
 if __name__ == '__main__':
     board = unruly.read_board(fileinput.input())
     solution = solve_board(board)
     board = minimize(board, solution)
+    blanks = sum(c == 2 for row in board for c in row)
+    print(f"Found a subset with {blanks} empty squares")
     print(unruly.board_to_str(board))
